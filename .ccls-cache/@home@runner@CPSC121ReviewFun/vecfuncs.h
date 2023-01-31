216674d23c23c97278ebf559d8e05aefbbefcb6b
jztennis <>
2023-01-31 20:42:28 +0000

    moved some stuff around

---
+++ .ccls-cache/@home@runner@CPSC121ReviewFun/vecfuncs.h
@@ -0,0 +1,15 @@
+//header file guard code prevents the problem of double inclusion of a header file.
+//see video in 121 playlist for more info
+
+#ifndef VECFUNCS_H 
+#define VECFUNCS_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void fillVector(vector<int>&); //prototype
+void findExtremes(vector<int>&, int&, int&);
+
+#endif
